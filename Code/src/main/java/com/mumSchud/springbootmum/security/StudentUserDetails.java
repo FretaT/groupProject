8e37bdc9cc3bae9549080273c47f4f94d4286396
Frehiwet Tsegay <ftsegay@miu.edu>
2022-07-13 14:26:25 -0500

    add

---
+++ Code/src/main/java/com/mumSchud/springbootmum/security/StudentUserDetails.java
@@ -0,0 +1,66 @@
+package com.mumSchud.springbootmum.security;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class StudentUserDetails implements UserDetails {
+
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private String username;
+    private String password;
+
+    public StudentUserDetails(String username, String password) {
+        super();
+        this.username = username;
+        this.password = password;
+    }
+
+    public StudentUserDetails() {
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_STUDENT"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
